1359fc98a1de4f8153bebb44dd53a5c809fb634f
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-07-13 13:19:18 -0400

    work

--- utils/gptFunctions.py
+++ utils/gptFunctions.py
@@ -146,6 +146,9 @@ def perform_google_query(
         return f"An error occurred: {e}"
 
 
+def perform_grok_query(context:str=system_message, query:str="Hi!", model:str="") -> str:
+    ...
+
 async def createQOTW(websites: str, model: str = "gpt-4o") -> str:
     if model not in gpt_models and model:
         return f"Invalid model. Please run the prompting help command to find the right model to use."
